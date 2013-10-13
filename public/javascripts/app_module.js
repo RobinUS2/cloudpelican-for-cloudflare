5ec1a09921d7bf6cb015e1b39931e5bba9a72a6b
Robin Verlangen <robin@us2.nl>
2013-10-13 18:34:23 +0200

    basic error listener

--- public/javascripts/app_module.js
+++ public/javascripts/app_module.js
@@ -1,1 +1,11 @@
-console.log('CloudPelican meets CloudFlare');
+/**
+* CloudPelican plugin for CloudFlare
+*/
+try {
+	console.log('CloudPelican meets CloudFlare');
+	window.onerror = function() {
+		console.log('error');
+	}
+} catch (e) {
+	window.console && console.log(e);
+}
